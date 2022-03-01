b52ed64c2620698f4e2f1ae20056e9cfdacd6fa9
rehabmohamed19 <rehabmohamedaziz99@gmail.com>
2022-03-01 07:05:35 -0800

    pages

---
+++ frontend/src/Pages/Guitar.js
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+
+const Guitar = () => {
+    return(
+        <>
+        <Card>
+    <Card.Img variant="top" src="https://online.berklee.edu/takenote/wp-content/uploads/2021/01/acoustic_guitar_techniques_article_image_2021.jpg" />
+    <Card.Body>
+      <Card.Text>
+        Guitar
+      </Card.Text>
+    </Card.Body>
+  </Card>
+    <div className="card mb-3">
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src="https://online.berklee.edu/takenote/wp-content/uploads/2021/01/acoustic_guitar_techniques_article_image_2021.jpg" className="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Title</h5>
+        <p className="card-text">Description</p>
+        <p className="card-text"><small class="text-muted">Reviews</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+         </>
+     );
+ }
+
+
+
+export default Guitar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Guitar from './Pages/Guitar';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Guitar/>
   </React.StrictMode>,
   document.getElementById('root')
 );
