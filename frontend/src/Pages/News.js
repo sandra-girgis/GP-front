a02b87c13781667eefdd30f4198c91584a55b8b0
sandra-girgis <totajo1999@gmail.com>
2022-03-07 21:07:45 +0200

    update

--- frontend/src/Pages/News.js
+++ frontend/src/Pages/News.js
@@ -24,45 +24,40 @@ class News extends Component {
     const news = this.state.NewsList
     return news.map(item => (
       <>
-        <div key={item.id} className="row g-0 ">
-            <div className="col-md-4 mt-4 rounded">
-              <img
-                src={require('../images'+item.picture.replaceAll('%20'," "))} 
-                className="img-fluid rounded-start"
-                alt="..."/>
-            </div>
-            <div className="col-md-6">
-              <div className="card-body ">
-                <h5 className="card-title ps-5 mt-1 ">
-                {item.title}
-                </h5>
-                <p className="card-text ps-5 new mt-4">
+        <div key={item.id} className="row flip_card2 ">
+          <div className="col-md-6 mt-4 rounded">
+            <img src={require('../images'+item.picture.replaceAll('%20'," "))} className="img-fluid rounded-start big" alt="..."/>
+          </div>
+          <div className="col-md-5">
+            <div className="card-body ">
+              <h5 className="card-title ps-3 mt-1 ">{item.title}</h5>
+              <p className="card-text ps-3 new mt-4">
                 {item.content}
-                </p>
-                <div>
-                  <span className="card-text ps-5 text-muted">
+              </p>
+              <div>
+                <span className="card-text ps-3 text-muted date">
                   {item.date.replaceAll("T"," ").replaceAll("Z"," ")}
-                  </span>
-                  &nbsp;&nbsp;&nbsp;&nbsp;
-                  <span className="btn btn-dark ms-4">
+                </span>
+                <span className='btn btn-dark ms-3'>
                   {item.Category_ID}
-                  </span>
-                </div>
+                </span>
               </div>
             </div>
           </div>
+        </div>
       </>
-    ));
-  };
+      )
+    )
+  }
+
   render() {
     return (
-      <>
-        <br></br>
-        <h1 className="new2">NEWS</h1>
-        <br></br>
-        <br></br>
+      <>     
+        <h1 className='new2 container'>
+          NEWS
+        </h1>
         <div className="container ">
-        {this.News()}
+          {this.News()}
         </div>
       </>
     );
