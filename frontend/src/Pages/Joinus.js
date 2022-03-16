81ae1af9bbf590ab9035e57107ae4de414317172
samah gaber <samahgabermo@gmail.com>
2022-03-16 16:55:00 +0200

    change join us form

--- frontend/src/Pages/Joinus.js
+++ frontend/src/Pages/Joinus.js
@@ -30,7 +30,37 @@ export const Joinus = () => {
     e.target.reset();
   };
 
- 
+  return (
+    <>
+      <div className="justify-content-center mb-5 row">
+        <div className="card shado col-md-5 p-5 mt-5 mb-5">
+          <h1 style={{ color: "#ee4a8b" }}>Send us a message</h1>
+          <Form className=" mt-3" ref={form} onSubmit={sendEmail}>
+            <Form.Group className="mb-3 " controlId="exampleForm.ControlInput1">
+              <Form.Label style={{ color: "#168eca" }}>Name</Form.Label>
+              <Form.Control type="text" name="name" />
+            </Form.Group>
+            <Form.Group className="mb-3 " controlId="exampleForm.ControlInput1">
+              <Form.Label style={{ color: "#168eca" }}>Email</Form.Label>
+              <Form.Control type="email" name="email" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label style={{ color: "#168eca" }}>Message</Form.Label>
+              <Form.Control name="message" as="textarea" rows={3} />
+            </Form.Group>
+            <Button
+              className="btn-outline-light btn-lg butt"
+              type="submit"
+              value="Send"
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
     </>
   );
 };
