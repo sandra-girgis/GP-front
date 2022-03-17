5ab5eb24395bc09f7859cc91b7afa98bbcd08249
omar <omar.akl14@gmail.com>
2022-03-17 16:43:08 +0200

    update

--- frontend/src/Pages/Admin/Addevent.js
+++ frontend/src/Pages/Admin/Addevent.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Form from "react-bootstrap/Form";
+import {Form, Col, InputGroup, Button} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
 import { NavLink } from "react-router-dom";
+import * as yup from "yup";
+import { Formik } from "formik";
 export const Addevent = () => {
+  const schema = yup.object().shape({
+    title: yup.string().required(),
+    content: yup.string().required(),
+    category: yup.string().required(),
+    picture: yup.string().required()
+  })
   const [picture, setPicture] = useState(null);
   const [title, setTitle] = useState(null);
   const [content, setContent] = useState(null);
@@ -42,41 +49,58 @@ export const Addevent = () => {
   };
   return (
     <div className="shado mb-5 pt-3 container">
-      <Form>
+      <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        title:"",
+        content:""
+      }}
+      >
+        {({handleSubmit, handleChange, values, touched, errors}) => (
+      <Form noValidate onSubmit={handleSubmit}>
         <h3 className="new2 pt-2 mb-5 text-center" style={{ color: "#ee4a8b" }}>
           Add Event
         </h3>
         <Form.Group
           className="mb-3 mt-5"
-          controlId="exampleForm.ControlTextarea1"
+          controlId="validationFormik01"
         >
           <Form.Label style={{ color: "#168eca" }}>title </Form.Label>
           <Form.Control
-            as="textarea"
-            rows={1}
-            // onChange={this.onChangeTitle}
-            // value={this.state.title}
+            type="text"
             name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            placeholder="title"
+            value ={values.title}
+            onChange={handleChange}
+            isValid={touched.title && !errors.title}
+            isInvalid={!!errors.title}
           />
+          <Form.Control.Feedback type="invalid">
+            {errors.title}
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group className="mb-3" controlId="validationFormik02">
           <Form.Label style={{ color: "#168eca" }}>content </Form.Label>
           <Form.Control
-            as="textarea"
-            rows={7}
+            type="text"
             name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            placeholder="content"
+            value={values.content}
+            onChange={handleChange}
+            isValid={touched.content && !errors.content}
+            isInvalid={!!errors.content}
           />
+          <Form.Control.Feedback type="invalid">
+            {errors.content}
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group
           className="mb-3 mt-5"
           controlId="exampleForm.ControlTextarea1"
         >
           <Form.Label style={{ color: "#168eca" }}>Category </Form.Label>
-          <Form.Control as="select" multiple onChange={handleChange}>
+          <Form.Control as="select" multiple onChange={handleChange} required>
             {cat.map((options) => (
               <option key={options.id} value={parseInt(options.id)}>
                 {options.name}
@@ -88,6 +112,7 @@ export const Addevent = () => {
           type="file"
           className="form-control"
           onChange={(e) => setPicture(e.target.files[0])}
+          required
         />
         <Button
           className="btn-outline-light btn-lg ms-5 mb-5 butt"
@@ -103,6 +128,8 @@ export const Addevent = () => {
           </NavLink>
         </Button>
       </Form>
+      )}
+      </Formik>
     </div>
   );
 };
