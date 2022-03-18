04b335e3c990917dc0e51cd94acdb23b8b76dcf5
omar <omar.akl14@gmail.com>
2022-03-18 14:36:19 +0200

    Validation Trial

--- frontend/src/Pages/Admin/Addevent.js
+++ frontend/src/Pages/Admin/Addevent.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Form, Col, InputGroup, Button} from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 export const Addevent = (props) => {
@@ -42,58 +42,41 @@ export const Addevent = (props) => {
   };
   return (
     <div className="shado mb-5 pt-3 container">
-      <Formik
-      validationSchema={schema}
-      onSubmit={console.log}
-      initialValues={{
-        title:"",
-        content:""
-      }}
-      >
-        {({handleSubmit, handleChange, values, touched, errors}) => (
-      <Form noValidate onSubmit={handleSubmit}>
+      <Form>
         <h3 className="new2 pt-2 mb-5 text-center" style={{ color: "#ee4a8b" }}>
           Add Event
         </h3>
         <Form.Group
           className="mb-3 mt-5"
-          controlId="validationFormik01"
+          controlId="exampleForm.ControlTextarea1"
         >
           <Form.Label style={{ color: "#168eca" }}>title </Form.Label>
           <Form.Control
-            type="text"
+            as="textarea"
+            rows={1}
+            // onChange={this.onChangeTitle}
+            // value={this.state.title}
             name="title"
-            placeholder="title"
-            value ={values.title}
-            onChange={handleChange}
-            isValid={touched.title && !errors.title}
-            isInvalid={!!errors.title}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
-          <Form.Control.Feedback type="invalid">
-            {errors.title}
-          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="validationFormik02">
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label style={{ color: "#168eca" }}>content </Form.Label>
           <Form.Control
-            type="text"
+            as="textarea"
+            rows={7}
             name="content"
-            placeholder="content"
-            value={values.content}
-            onChange={handleChange}
-            isValid={touched.content && !errors.content}
-            isInvalid={!!errors.content}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
           />
-          <Form.Control.Feedback type="invalid">
-            {errors.content}
-          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group
           className="mb-3 mt-5"
           controlId="exampleForm.ControlTextarea1"
         >
           <Form.Label style={{ color: "#168eca" }}>Category </Form.Label>
-          <Form.Control as="select" multiple onChange={handleChange} required>
+          <Form.Control as="select" multiple onChange={handleChange}>
             {cat.map((options) => (
               <option key={options.id} value={parseInt(options.id)}>
                 {options.name}
@@ -105,7 +88,6 @@ export const Addevent = (props) => {
           type="file"
           className="form-control"
           onChange={(e) => setPicture(e.target.files[0])}
-          required
         />
         <Button
           className="btn-outline-light btn-lg ms-5 mb-5 butt"
@@ -115,8 +97,6 @@ export const Addevent = (props) => {
             Submit
         </Button>
       </Form>
-      )}
-      </Formik>
     </div>
   );
 };
--- frontend/src/Pages/Admin/Addinstructor.js
+++ frontend/src/Pages/Admin/Addinstructor.js
@@ -1,8 +1,9 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
+
 
 export const Addinstructor = (props) => {
 const [picture, setPicture] = useState(null);
@@ -15,15 +16,15 @@ const [bio, setBio] = useState(null);
 
 const addNewStudent = async () => {
     let formField = new FormData();
-    formField.append("username", username);
-    formField.append("password", password);
-    formField.append("email", email);
-    formField.append("phoneNumber", phoneNumber);
-    formField.append("salary", salary);
-    formField.append("bio", bio);
+    formField.append("username", formValues.username);
+    formField.append("password", formValues.password);
+    formField.append("email", formValues.email);
+    formField.append("phoneNumber", formValues.phoneNumber);
+    formField.append("salary", formValues.salary);
+    formField.append("bio", formValues.bio);
     formField.append("is_staff", true);
     if (picture !== null) {
-    formField.append("picture", picture);
+    formField.append("picture", formValues.picture);
     }
     await axios({
     method: "post",
@@ -34,9 +35,52 @@ const addNewStudent = async () => {
     });
     props.history.push("/instructors");
 };
+
+const initialValues = {username: "", email: "", password: "", phoneNumber:"", salary:"", bio:"", picture:""};
+const [formValues, setFormValues] = useState(initialValues);
+const [formErrors, setFormErrors] = useState({});
+const [isSubmit, setIsSubmit] = useState(false);
+
+const handleChange = (e) => {
+//    console.log(e.target);
+    const { name, value } = e.target;
+    setFormValues({...formValues, [name]:value});
+    console.log(formValues);
+};
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+};
+
+useEffect(() => {
+    console.log(formErrors)
+    if(isSubmit){
+        console.log(formValues);
+        //setEmail(formValues.email)
+        addNewStudent();
+    }
+},[formErrors])
+
+const validate = (values) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if(!values.username){
+        errors.username = "Username is required.";
+    }
+    if(!values.email){
+        errors.email = "Email is required.";
+    }
+    if(!values.password){
+        errors.password = "Password is required.";
+    }
+    return errors;
+};
+
 return (
     <div className="shado mb-5 pt-3 container">
-    <Form>
+    <Form onSubmit={handleSubmit}>
         <h3 className="new2 pt-2 mb-5 text-center" style={{ color: "#ee4a8b" }}>
         Add Instructor
         </h3>
@@ -49,9 +93,12 @@ return (
             as="input"
             placeholder="Enter Instructor Name"
             name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            //value={username}
+            //onChange={(e) => setUsername(e.target.value)}
+            value={formValues.username}
+            onChange={handleChange}
         />
+        <p>{ formErrors.username }</p>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
@@ -61,12 +108,15 @@ return (
             placeholder="Enter email"
             as="input"
             name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            //value={email}
+           // onChange={(e) => setEmail(e.target.value)}
+           value={formValues.email}
+           onChange={handleChange}
         />
         <Form.Text className="text-muted">
             We'll never share your email with anyone else.
         </Form.Text>
+        <p>{ formErrors.email }</p>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label style={{ color: "#168eca" }}>Password</Form.Label>
@@ -74,9 +124,12 @@ return (
             as="input"
             placeholder="Enter Password"
             name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            //value={password}
+            //onChange={(e) => setPassword(e.target.value)}
+            value={formValues.password}
+            onChange={handleChange}
         />
+        <p>{ formErrors.password }</p>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlNumber">
         <Form.Label style={{ color: "#168eca" }}>Phone Number</Form.Label>
@@ -85,8 +138,10 @@ return (
             type="text"
             placeholder="Enter Phone Number"
             name="phoneNumber"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
+            value={formValues.phoneNumber}
+            //onChange={(e) => setPhoneNumber(e.target.value)}
+            
+            onChange={handleChange}
         />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
@@ -97,8 +152,9 @@ return (
             type="text"
             placeholder="Enter Bio"
             name="bio"
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
+            value={formValues.bio}
+            //onChange={(e) => setBio(e.target.value)}
+            onChange={handleChange}
         />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlNumber">
@@ -109,19 +165,22 @@ return (
             type="number"
             placeholder="Enter Salary"
             name="salary"
-            value={salary}
-            onChange={(e) => setSalary(e.target.value)}
+            value={formValues.salary}
+            //onChange={(e) => setSalary(e.target.value)}
+            onChange={handleChange}
         />
         </Form.Group>
         <input
         type="file"
         className="form-control"
-        onChange={(e) => setPicture(e.target.files[0])}
+        //onChange={(e) => setPicture(e.target.files[0])}
+        onChange={handleChange}
+        //value = {formValues.picture}
         />
         <Button
         className="btn-outline-light btn-lg ms-5 mb-5 butt"
         style={{ backgroundColor: "#168eca" }}
-        onClick={addNewStudent}
+        //onClick={addNewStudent}
         >
             Submit
         </Button>
